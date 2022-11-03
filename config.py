8486b38382c2e57360108db49446f5148c1c08e6
Liza Zhyrova <yelyzaveta1608@gmail.com>
2022-11-03 10:31:58 +0200

    Add api key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Paste your API key here
-API_KEY = ""
+API_KEY = "ZmQ2ODMzM2MtZjdhNy00Yjg0LTk1ZjgtMGQ1MjA1ZTNiMjlm"
 BASE_ENDPOINT = "https://api.clockify.me/api/v1/"
 
 data = {'x-api-key': API_KEY}
